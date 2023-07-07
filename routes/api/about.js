ed0dda844f0f662255abad4ef31e87d795bda820
Pape Ndiaye <sagehigh@yahoo.com>
2023-07-06 22:52:45 -0400

    Routes Mounted

---
+++ routes/api/about.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+// @route   GET api/about
+// @desc    Test route
+// @access  Public
+router.get("/", (req, res) => {
+    res.send("About route");
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const aboutRoute = require("./routes/api/about");
+const authRoute = require("./routes/api/auth");
+const profileRoute = require("./routes/api/profile");
+const usersRoute = require("./routes/api/users");
 
 const app = express();
 
@@ -9,6 +13,12 @@ connectDB();
 app.get("/", (req, res) => {
     res.send("API is running");
 });
+
+// Mount Routes
+app.use("/api/about", aboutRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/profile", profileRoute);
+app.use("/api/users", usersRoute);
 
 const PORT = process.env.PORT || 5000;
 
