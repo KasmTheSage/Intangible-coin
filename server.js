107c2a74d1b0b41522eec38766b732b5b23760c8
Pape Ndiaye <sagehigh@yahoo.com>
2023-09-29 17:30:50 -0400

    Prepare for deployment

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const profileRoute = require("./routes/api/profile");
 const usersRoute = require("./routes/api/users");
 const cron = require("node-cron");
 const User = require("./models/User");
+const path = require("path");
 
 const app = express();
 
@@ -15,15 +16,22 @@ connectDB();
 // Body parser
 app.use(express.json({ extended: false}));
 
-app.get("/", (req, res) => {
-    res.send("API is running");
-});
-
 // Mount Routes
 app.use("/api/about", aboutRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/profile", profileRoute);
 app.use("/api/users", usersRoute);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+   app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+   });
+};
+
 
 // Cron job to update the coin balance every hour
 cron.schedule('0 * * * *', async () => {
